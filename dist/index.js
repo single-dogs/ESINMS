360a9bd0ca49d949031b7cd8f49e947080c09a3c
GaoLiHai <1019933576@qq.com>
2021-04-26 18:55:31 +0800

    chore: 修改 git hook 'npm add dist' -> 'git add dist'

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,38 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var koa_1 = __importDefault(require("koa"));
-var server = new koa_1.default();
-server.use(function (ctx) {
-    ctx.body = 'hello';
-});
-server.listen(80);
+
+var _koa = _interopRequireDefault(require("koa"));
+
+var _koaLogger = _interopRequireDefault(require("koa-logger"));
+
+var _koaConditionalGet = _interopRequireDefault(require("koa-conditional-get"));
+
+var _koaEtag = _interopRequireDefault(require("koa-etag"));
+
+var _koaSession = _interopRequireDefault(require("koa-session"));
+
+var _koaStatic = _interopRequireDefault(require("koa-static"));
+
+var _koaRouter = _interopRequireDefault(require("koa-router"));
+
+var _path = _interopRequireDefault(require("path"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// koa
+// util
+// app
+const server = new _koa.default(); // main
+
+server // dev logger
+.use((0, _koaLogger.default)()) // 304 res
+.use((0, _koaConditionalGet.default)()).use(_koaEtag.default) // static
+.use((0, _koaStatic.default)(_path.default.resolve(__dirname, 'public'), {
+  defer: false
+})) // session
+.use((0, _koaSession.default)({
+  key: 'sk'
+}, server)) // router
+.use(ctx => {
+  ctx.body = 'hello';
+}).use(new _koaRouter.default().allowedMethods()) // listen
+.listen(80);
