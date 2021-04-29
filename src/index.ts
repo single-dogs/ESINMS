e26c0b707bd16bab5b24b8d4ffbad28f32cb4ec1
GaoLiHai <1019933576@qq.com>
2021-04-29 14:35:51 +0800

    feature: 中间件结构

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
-import Koa, { Context } from 'koa';
+// koa
+import Koa from 'koa'
+import koaLogger from 'koa-logger'
+import koaConditionalGet from 'koa-conditional-get'
+import koaEtag from 'koa-etag'
+import koaSession from 'koa-session'
+import koaStatic from 'koa-static'
+import koaRouter from 'koa-router'
 
-const server: Koa = new Koa();
+// rest router
+import { router } from './router'
 
-server.use((ctx: Context) => {
-  ctx.body = 'hello';
-});
+// util
+import path from 'path'
 
-server.listen(80);
+// app
+const server: Koa = new Koa()
+
+// main
+server
+  // dev logger
+  .use(koaLogger())
+
+  // 304 res
+  .use(koaConditionalGet())
+  .use(koaEtag())
+
+  // static
+  .use(koaStatic(path.resolve(__dirname, 'public'), { defer: false }))
+
+  // session
+  .use(koaSession({ key: 'sk', }, server))
+
+  // router
+  .use(router.routes())
+  .use(new koaRouter().allowedMethods())
+
+  // listen
+  .listen(80)
