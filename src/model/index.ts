4707edcca75b4c6d62e8afc571b2b0054df7b453
GaoLiHai <1019933576@qq.com>
2021-04-29 14:34:15 +0800

    feature: 数据层

---
+++ src/model/index.ts
@@ -0,0 +1,39 @@
+import { MongoClient, MongoError } from 'mongodb'
+import config from '../config'
+
+// 数据库配置
+const { db: { uri, name } } = config.get()
+
+// MongoClient
+const client = new MongoClient(uri, { useUnifiedTopology: true })
+
+// 连接 mongo 服务器
+client.connect((err: MongoError) => {
+  if (err) {
+    return console.log('数据库连接异常', err)
+  }
+  console.log('数据库连接成功')
+})
+
+// 数据库
+const db = client.db(name)
+
+// 表
+const collections = {
+  Worker: db.collection('worker'),
+  Vehicle: db.collection('vehicle'),
+}
+
+/**
+ * 模型约束
+ * - worker id 升序唯一索引
+ * - vehicle id 升序唯一索引
+ */
+collections.Worker.createIndex({ id: 1 }, { unique: true }, console.log)
+collections.Vehicle.createIndex({ id: 1 }, { unique: true }, console.log)
+
+export {
+  client,
+  db,
+  collections,
+}
