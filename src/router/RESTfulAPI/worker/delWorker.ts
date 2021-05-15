c74393b5e41669dcc2368c22c5701071b542779c
GaoLiHai <1019933576@qq.com>
2021-05-15 16:01:50 +0800

    feature: 理由层 worker 删除的实现

---
+++ src/router/RESTfulAPI/worker/delWorker.ts
@@ -0,0 +1,36 @@
+import { Context } from 'koa'
+import { collections } from '../../../model/DB'
+import { errorHandler } from '../../../util/errorHandler'
+import joi from 'joi'
+import { pick } from 'lodash'
+
+// check
+const paramsSchema = joi.object({
+  id: joi.number().required(),
+})
+
+export async function delWorker(ctx: Context): Promise<void> {
+  try {
+    const {
+      value: params,
+      error,
+    } = paramsSchema.validate(ctx.query)
+
+    if (error) {
+      throw new Error(error.message)
+    }
+
+    await collections.Worker.deleteOne(pick(params, ['id']))
+
+    ctx.manager.delWorker(params.id)
+
+    ctx.body = {
+      code: 0,
+      data: {
+        deleted: { id: params.id }
+      }
+    }
+  } catch (error) {
+    errorHandler(error, ctx)
+  }
+}
