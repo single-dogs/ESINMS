c3f77d242222b82eac176a96a073891b073867e5
GaoLiHai <1019933576@qq.com>
2021-05-15 10:55:52 +0800

    refactor: 设备数据上报抽象重构，统一拉取缓存，避免请求压力过大

--- src/model/Data/index.ts
+++ src/model/Data/index.ts
@@ -1,3 +1,5 @@
+import { pick } from 'lodash'
+
 export interface DeviceData {
   // 经度
   lon: number;
@@ -8,8 +10,43 @@ export interface DeviceData {
   // 在线
   online: boolean;
 }
+interface SourceDeviceData {
+  // 设备号
+  device: number;
 
-export async function pull(device: number): Promise<DeviceData> {
-  device
-  return { lon: 0, lat: 0, online: true } as DeviceData
+  // 经度
+  lon: number;
+
+  // 维度
+  lat: number;
+
+  // 在线
+  online: boolean;
+}
+function pullFromSource(): SourceDeviceData[] {
+  // from source
+  return [
+    { device: 0, lon: 1, lat: 2, online: true },
+    { device: 1, lon: 1, lat: 2, online: true },
+    { device: 2, lon: 1, lat: 2, online: true },
+    { device: 3, lon: 1, lat: 2, online: true },
+    { device: 4, lon: 1, lat: 2, online: true },
+    { device: 5, lon: 1, lat: 2, online: false },
+    { device: 6, lon: 1, lat: 2, online: true },
+    { device: 7, lon: 1, lat: 2, online: true },
+    { device: 8, lon: 1, lat: 2, online: true },
+  ]
+}
+
+const dataMap = new Map<number, DeviceData>()
+
+setInterval(() => {
+  pullFromSource().forEach(data => {
+    dataMap.set(data.device, pick(data, ['lon', 'lat', 'online']))
+  })
+}, 1000)
+
+
+export async function pull(device: number): Promise<DeviceData | undefined> {
+  return dataMap.get(device)
 }
